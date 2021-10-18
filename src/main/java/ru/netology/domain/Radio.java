b5dc6c0f9180230708c525d402946eac764db9d2
PetrIvChe <cherniavskiipetr@gmail.com>
2021-10-18 12:33:37 +0300

    Added mothods of decrease and increase of volume and tests

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -3,11 +3,23 @@ package ru.netology.domain;
 public class Radio {
     String name;
     int currentStation;
-    int currentVolume;
+    private int currentVolume;
 
 
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
 
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
 
+    }
     //Метод увеличения звука на 1
     public void increaseVolume() {
         if (currentVolume < 10) {
@@ -15,22 +27,33 @@ public class Radio {
         }
     }
 
+
     //Метод уменьшения звука на 1
     public void decreaseVolume() {
-        if (currentVolume < 0) {
+        if (currentVolume > 0) {
             currentVolume = currentVolume - 1;
         }
     }
-
-    //Метод переключения станции next
-    //Номер текущей радиостанции изменяется в пределах от 0 до 9
-    public void nextStation() {
-        if (currentStation < 0) {
-            currentStation = currentStation + 1;
-        }
-    }
-
-    public void  prevStation() {
-        if (currentStation )
-    }
 }
+//Метод переключения станции next
+//Номер текущей радиостанции изменяется в пределах от 0 до 9
+//public void nextStation(){
+//        if(currentStation< 0){
+//        currentStation=currentStation+1;
+//        if(currentStation==9){
+//        currentStation=0;
+//        }
+//        }
+//        }
+//
+////Метод переключения станции prev
+////Номер текущей радиостанции изменяется в пределах от 9 до 0
+//public void prevStation(){
+//        if(currentStation>0){
+//        currentStation=currentStation-1;
+//        if(currentStation==0){
+//        currentStation=9;
+//        }
+//        }
+//        }
+//        }
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,19 +1,45 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class RadioTest {
 
     @Test
-    public void increaseVolumeTest() {
-        CurrentVolume CurrentVolume = new CurrentVolume();
-
+    //Граничные значения 0
+    public void shouldReturnIncreaseVolume0Test() {
+        Radio currentV = new Radio();
+        currentV.setCurrentVolume(0);
+        currentV.increaseVolume();
+        int actual = currentV.getCurrentVolume();
+        int expected = 1;
+        assertEquals(expected, actual);
     }
 
     @Test
-    void decreaseVolume() {
+    //Граничные значения 10
+    public void shouldReturnIncreaseVolume10Test() {
+        Radio currentV = new Radio();
+        currentV.setCurrentVolume(11);
+        currentV.increaseVolume();
+        int actual = currentV.getCurrentVolume();
+        int expected = 10;
+        assertEquals(expected, actual);
     }
+
+    @Test
+    //Граничные значения 0
+    public void shouldReturnDecreaseVolume0Test() {
+        Radio currentV = new Radio();
+        currentV.setCurrentVolume(0);
+        currentV.decreaseVolume();
+        int actual = currentV.getCurrentVolume();
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+
 }
